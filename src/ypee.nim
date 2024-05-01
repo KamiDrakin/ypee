e9c93dde02d7e19c4a0a479de3d42af4dc86b099
Drakin Kami <kamidrakin@gmail.com>
2024-05-01 12:09:17 +0200

    Frame counter.

--- src/ypee.nim
+++ src/ypee.nim
@@ -4,7 +4,8 @@ import ypee_eg
 import glm
 import glad/gl
 import glrenderer
-import random
+#import random
+#from glfw import getTime
 
 proc main() =
     var eg: YpeeEg
@@ -31,25 +32,31 @@ proc main() =
         testImage: GLImage
         testShape: GLShape
     #testSprite.init("./textures/checker.bmp")
-    testImage.init("./textures/temp.bmp")
+    testImage.init("./textures/font.bmp")
     testShape.init(eg.renderer.program(0), testVertices, testIndices)
 
-    glViewport(0, 0, 256 * 3, 240 * 3)
+    #glViewport(0, 0, 256 * 3, 240 * 3)
 
     while eg.nextFrame():
         eg.processEvents()
+
+        if eg.frameCounter.elapsed >= 3.0:
+            echo eg.frameCounter.getFps()
 
         glClearColor(0.1, 0.0, 0.1, 1.0)
         glClear((GL_COLOR_BUFFER_BIT.uint + GL_DEPTH_BUFFER_BIT.uint).GLbitfield)
     
         #for i in countup(0, 100):
+        #let
+        #    r1 = rect(16.0 * float(rand(13)), 16.0 * float(rand(6)), 32.0, 32.0)
+        #    r2 = rect(16.0 * float(rand(13)), 16.0 * float(rand(6)), 16.0, 16.0)
+        #    m1 = mat4f().translate(-1.0 + rand(2.0), -1.0 + rand(2.0), 0.0)
+        #    m2 = mat4f().translate(-1.0 + rand(2.0), -1.0 + rand(2.0), 0.0)
         let
-            r1 = rect(16.0 * float(rand(13)), 16.0 * float(rand(6)), 32.0, 32.0)
-            r2 = rect(16.0 * float(rand(13)), 16.0 * float(rand(6)), 16.0, 16.0)
-            m1 = mat4f().translate(-1.0 + rand(2.0), -1.0 + rand(2.0), 0.0)
-            m2 = mat4f().translate(-1.0 + rand(2.0), -1.0 + rand(2.0), 0.0)
-        #let r = rect(0.0, 0.0, 16.0, 16.0)
-        #let r = rect(0.0, 0.0, 240.0, 128.0)
+            r1 = rect(32.0, 24.0, 16.0, 16.0)
+            r2 = rect(64.0, 64.0, 64.0, 64.0)
+            m1 = mat4f().translate(-0.5, 0.0, 0.0)
+            m2 = mat4f().translate(0.5, 0.0, 0.0)
         eg.renderer.draw(testShape, testImage, instance(r1, m1))
         eg.renderer.draw(testShape, testImage, instance(r2, m2))
         #eg.renderer.draw(testShape, testImage)
--- src/ypee_eg.nim
+++ src/ypee_eg.nim
@@ -1,3 +1,5 @@
+import std/sequtils
+
 import glm
 import glfw
 
@@ -12,11 +14,38 @@ const
 type
     Programs* = enum
         prBase
+    FrameCounter = object
+        frameCounts: seq[int]
+        prevTime: float
+        frameTimer: float
+        elapsed*: float
     Sprite* = object
-        image: GLImage
     YpeeEg* = object
         window: glfw.Window
         renderer*: GLRenderer
+        frameCounter*: FrameCounter
+
+proc init(fc: var FrameCounter) =
+    fc.frameCounts = @[0]
+    fc.frameTimer = 0.0
+    fc.elapsed = 0.0
+
+proc tick(fc: var FrameCounter) =
+    fc.frameCounts[fc.frameCounts.len() - 1] += 1
+    let time = glfw.getTime()
+    fc.frameTimer += time - fc.prevTime
+    fc.prevTime = time
+    if fc.frameTimer >= 1.0:
+        fc.frameTimer -= 1.0
+        fc.elapsed += 1.0
+        fc.frameCounts.add(0)
+    if fc.elapsed >= 100.0:
+        fc.init()
+
+proc getFps*(fc: var FrameCounter): float =
+    let frameSum = fc.frameCounts.foldl(a + b)
+    result = frameSum.float / fc.elapsed
+    fc.init()
         
 proc refreshProjection*(eg: var YpeeEg) =
     let windowSize = eg.window.size()
@@ -42,24 +71,24 @@ proc init*(eg: var YpeeEg) =
         fShaderSrc = staticRead("shaders/ypee.fs")
         uniforms = @[
             "texSize",
-            #"texRect",
             "viewMat",
             "projMat",
         ]
     eg.renderer.addProgram(prBase.uint, vShaderSrc, fShaderSrc, uniforms)
-    eg.renderer.setViewMat(mat4[float32]())
-    #eg.renderer.setProjMat(ortho[float32](0.0, screenSize[0].float, 0.0, screenSize[1].float, -1.0, 1000.0))
+    eg.renderer.setViewMat(mat4f())
     eg.refreshProjection()
-    #eg.renderer.setViewMat(mat4[float32]());
     
     eg.window.registerWindowCallbacks()
     glfw.swapInterval(1)
+
+    eg.frameCounter.init()
 
 proc destroy*(eg: var YpeeEg) =
     eg.window.destroy()
     glfw.terminate()
     
 proc nextFrame*(eg: var YpeeEg): bool =
+    eg.frameCounter.tick()
     glfw.swapBuffers(eg.window)
     if windowSizeChanged:
         eg.refreshProjection()
